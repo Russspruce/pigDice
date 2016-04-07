57c061aa38c0b725b554583ecbd695f6cc18e446
Andrew Russo and Kevin Deganos <andrew@>
2016-04-07 09:26:40 -0700

    Changed code for single dice pig game to Objects to streamline code.

--- scripts.js
+++ scripts.js
@@ -1,10 +1,10 @@
 function Score() {
   this.tempScore = 0;
   this.totalScore = 0;
-
+  this.turnOrder = 1;
 }
 
-function Replay() {
+function Replay() { //Gives message for replay that will refresh page
   var response = confirm("Would you like to play again?");
    if(response)location.reload();
 }
@@ -14,18 +14,35 @@ Score.prototype.addTotal = function(tempScore) {
 
 }
 
+Score.prototype.playerTurn = function(current, randRoll) { //goes through roll and calculates points
+  if (randRoll === 1) {
+    this.tempScore = 0;
+    currentPlayer = (currentPlayer * -1);
+    $("#currentSubTotal").text(this.tempScore);
+  } else {
+    this.addTemp(randRoll);
+    $("#currentSubTotal").text(this.tempScore);
+  }
+  return currentPlayer; //determines what player is currently active
+}
+
+Score.prototype.stopTurn = function() { //stops and stores dice rolls into total score
+  this.addTotal(this.tempScore);
+  this.tempScore = 0;
+  $("#currentSubTotal").text(0);
+}
+
 Score.prototype.addTemp = function(randRoll) {
   this.tempScore += randRoll;
 
 }
-
+  var currentPlayer = 1;
 
 $(document).ready(function() {
   $("button#rulesButton").click(function() {
     $('#rulesPigDice').toggle();
 });
   $("#currentPlayerTwo").hide();
-  var currentPlayer = 1;
   var p1 = new Score();
   var p2 = new Score();
   var randRoll = 0;
@@ -35,29 +52,18 @@ $(document).ready(function() {
      randRoll = 1 + Math.floor(Math.random() * 6);
       $("#currentDiceRoll").text(randRoll);
 
-
     if (currentPlayer === 1) {
-      if (randRoll === 1) {
-        p1.tempScore = 0;
-        currentPlayer = 2;
-        $("#currentSubTotal").text(p1.tempScore);
+      currentPlayer = p1.playerTurn(currentPlayer, randRoll);
+      if (currentPlayer === -1) {
         $("#currentPlayerOne").hide();
         $("#currentPlayerTwo").show();
-      } else {
-        p1.addTemp(randRoll);
-        $("#currentSubTotal").text(p1.tempScore);
       }
     }
       else {
-        if (randRoll === 1) {
-          p2.tempScore = 0;
-          currentPlayer = 1;
-          $("#currentSubTotal").text(p2.tempScore);
+        currentPlayer = p2.playerTurn(currentPlayer, randRoll);
+        if (currentPlayer === 1) {
           $("#currentPlayerTwo").hide();
           $("#currentPlayerOne").show();
-        } else {
-          p2.addTemp(randRoll);
-          $("#currentSubTotal").text(p2.tempScore);
         }
       }
 
@@ -65,31 +71,26 @@ $(document).ready(function() {
   });
 
   $("#stopRoll").click(function(event) {
-
     if (currentPlayer === 1) {
-      p1.addTotal(p1.tempScore);
-      p1.tempScore = 0;
+      p1.stopTurn();
       $("#playerOneScore").text(p1.totalScore);
-      $("#currentSubTotal").text(0);
-      currentPlayer = 2;
+      currentPlayer = -1;
       $("#currentPlayerOne").hide();
       $("#currentPlayerTwo").show();
         if (p1.totalScore >= 50) {
-          alert("Yay!")
+          alert("Congratulations, Player One!  You have claimed victory in this game of pigs!  Bravo!")
           Replay();
 
         }
 
     } else {
-      p2.addTotal(p2.tempScore)
-      p2.tempScore = 0;
+      p2.stopTurn();
       $("#playerTwoScore").text(p2.totalScore);
-      $("#currentSubTotal").text(0);
       currentPlayer = 1;
       $("#currentPlayerTwo").hide();
       $("#currentPlayerOne").show();
         if (p2.totalScore >= 50) {
-          alert("Huzzah!")
+          alert("Awesome, Player Two!  You have mastered this game and are the reigning champion!")
           Replay();
         }
     }
